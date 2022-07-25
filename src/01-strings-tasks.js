f2384aa0223d8aba4c44f3d9e4c51a4e3c63e9ad
sevc4ik <31854320+sevc4ik@users.noreply.github.com>
2022-07-25 16:46:25 +0300

    feat: complete tasks string

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -18,9 +18,7 @@
  *   'aa',''    => 'aa'
  *   '',  'bb'  => 'bb'
  */
-function concatenateStrings(/* value1, value2 */) {
-  throw new Error('Not implemented');
-}
+const concatenateStrings = (str1, str2) => str1 + str2;
 
 
 /**
@@ -34,9 +32,7 @@ function concatenateStrings(/* value1, value2 */) {
  *   'b'     => 1
  *   ''      => 0
  */
-function getStringLength(/* value */) {
-  throw new Error('Not implemented');
-}
+const getStringLength = (str) => str.length;
 
 /**
  * Returns the result of string template and given parameters firstName and lastName.
@@ -51,10 +47,7 @@ function getStringLength(/* value */) {
  *   'John','Doe'      => 'Hello, John Doe!'
  *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
  */
-function getStringFromTemplate(/* firstName, lastName */) {
-  throw new Error('Not implemented');
-}
-
+const getStringFromTemplate = (firstName, lastName) => `Hello, ${firstName} ${lastName}!`;
 /**
  * Extracts a name from template string 'Hello, First_Name Last_Name!'.
  *
@@ -65,10 +58,7 @@ function getStringFromTemplate(/* firstName, lastName */) {
  *   'Hello, John Doe!' => 'John Doe'
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
-}
-
+const extractNameFromTemplate = (val) => val.replace('Hello, ', '').replace('!', '');
 
 /**
  * Returns a first char of the given string.
@@ -80,9 +70,7 @@ function extractNameFromTemplate(/* value */) {
  *   'John Doe'  => 'J'
  *   'cat'       => 'c'
  */
-function getFirstChar(/* value */) {
-  throw new Error('Not implemented');
-}
+const getFirstChar = (val) => val[0];
 
 /**
  * Removes a leading and trailing whitespace characters from string.
@@ -95,9 +83,7 @@ function getFirstChar(/* value */) {
  *   'cat'              => 'cat'
  *   '\tHello, World! ' => 'Hello, World!'
  */
-function removeLeadingAndTrailingWhitespaces(/* value */) {
-  throw new Error('Not implemented');
-}
+const removeLeadingAndTrailingWhitespaces = (str) => str.trim();
 
 /**
  * Returns a string that repeated the specified number of times.
@@ -110,9 +96,7 @@ function removeLeadingAndTrailingWhitespaces(/* value */) {
  *   'A', 5  => 'AAAAA'
  *   'cat', 3 => 'catcatcat'
  */
-function repeatString(/* value, count */) {
-  throw new Error('Not implemented');
-}
+const repeatString = (str, count) => str.repeat(count);
 
 /**
  * Remove the first occurrence of string inside another string
@@ -126,10 +110,7 @@ function repeatString(/* value, count */) {
  *   'I like legends', 'end' => 'I like legs',
  *   'ABABAB','BA' => 'ABAB'
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
-}
-
+const removeFirstOccurrences = (str, value) => str.replace(value, '');
 /**
  * Remove the first and last angle brackets from tag string
  *
@@ -141,9 +122,7 @@ function removeFirstOccurrences(/* str, value */) {
  *   '<span>' => 'span'
  *   '<a>' => 'a'
  */
-function unbracketTag(/* str */) {
-  throw new Error('Not implemented');
-}
+const unbracketTag = (str) => str.replace(/<|>/g, '');
 
 
 /**
@@ -156,9 +135,7 @@ function unbracketTag(/* str */) {
  *   'Thunderstruck' => 'THUNDERSTRUCK'
  *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
  */
-function convertToUpperCase(/* str */) {
-  throw new Error('Not implemented');
-}
+const convertToUpperCase = (str) => str.toUpperCase();
 
 /**
  * Extracts e-mails from single string with e-mails list delimeted by semicolons
@@ -175,9 +152,7 @@ function convertToUpperCase(/* str */) {
  *   ],
  *   'info@gmail.com' => ['info@gmail.com']
  */
-function extractEmails(/* str */) {
-  throw new Error('Not implemented');
-}
+const extractEmails = (str) => str.split(';');
 
 /**
  * Returns the string representation of rectangle with specified width and height
@@ -202,9 +177,20 @@ function extractEmails(/* str */) {
  *             '└──────────┘\n'
  *
  */
-function getRectangleString(/* width, height */) {
-  throw new Error('Not implemented');
-}
+const getRectangleString = (width, height) => {
+  const topRec = `┌${'─'.repeat(width - 2)}┐\n`;
+  const midRec = `│${' '.repeat(width - 2)}│\n`;
+  const botRec = `└${'─'.repeat(width - 2)}┘\n`;
+
+  switch (height) {
+    case 1:
+      return topRec;
+    case 2:
+      return topRec + botRec;
+    default:
+      return topRec + midRec.repeat(height - 2) + botRec;
+  }
+};
 
 
 /**
@@ -223,10 +209,22 @@ function getRectangleString(/* width, height */) {
  *    => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
  *
  */
-function encodeToRot13(/* str */) {
-  throw new Error('Not implemented');
-}
+const encodeToRot13 = (str) => {
+  const alph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  const alphROT13 = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+  let result = '';
 
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i].match(/[a-zA-Z]/)) {
+      const index = alph.indexOf(str[i]);
+      result += alphROT13[index];
+    } else {
+      result += str[i];
+    }
+  }
+
+  return result;
+};
 /**
  * Returns true if the value is string; otherwise false.
  * @param {string} value
@@ -240,9 +238,7 @@ function encodeToRot13(/* str */) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
-}
+const isString = (str) => typeof str === 'string' || str instanceof String;
 
 
 /**
@@ -269,9 +265,17 @@ function isString(/* value */) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
-}
+
+const getCardId = (str) => {
+  const deck = [
+    'A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣',
+    'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦',
+    'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥',
+    'A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠',
+  ];
+
+  return deck.indexOf(str);
+};
 
 
 module.exports = {
